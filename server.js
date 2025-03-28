d822d0e4db4a0019f4f132d463ec8b856c90b554
tarbipyakurel21 <tarbipyakurel21@gmail.com>
2025-03-27 21:40:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: "https://todo-app-frontend-vdh3.vercel.app", // Allow frontend
+  origin: "https://todo-app-frontend-wwv4.vercel.app", // Allow frontend
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"],
 }));
