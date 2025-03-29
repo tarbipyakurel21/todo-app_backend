6400a53c9fc9cccaacbf37ce746eac0d5df9016e
Tarbi Pyakurel <tarbipyakurel@Tarbis-MacBook-Air.local>
2025-03-29 11:34:00 -0500

    Updated

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -6,7 +6,7 @@ const authenticate=require("../middleware/authMiddleware")
 //Get all todos only for logged in-user
 router.get("/", authenticate, async (req, res) => {
     try {
-        const todos = await Todo.find({user:req.user.id});
+        const todos = await Todo.find({userId:req.user.id});
         res.json(todos);
       } catch (err) {
         res.status(500).json({ error: err.message });
@@ -18,7 +18,7 @@ router.get("/", authenticate, async (req, res) => {
 router.post("/", authenticate, async (req, res) => {
     console.log("Request Body:", req.body);
     try {
-        const newTodo = new Todo({ ...req.body, user: req.user.id });
+        const newTodo = new Todo({ ...req.body, userId: req.user.id });
         await newTodo.save();
         res.status(201).json(newTodo);
     } catch (err) {
@@ -35,7 +35,7 @@ router.put("/:id",authenticate, async (req, res) => {
     return res.status(400).json({ message: "Invalid Todo ID" });
 }  
   try {
-        const updatedTodo = await Todo.findOneAndUpdate({_id:req.params.id,user:req.user.id},req.body, { new: true });
+        const updatedTodo = await Todo.findOneAndUpdate({_id:req.params.id,userId:req.user.id},req.body, { new: true });
         if (!updatedTodo) return res.status(404).json({ message: "Todo not found" });
         res.json(updatedTodo);
       } catch (err) {
@@ -47,7 +47,7 @@ router.put("/:id",authenticate, async (req, res) => {
 
 router.delete("/:id",authenticate, async (req, res) => {
     try {
-        const deletedTodo = await Todo.findOneAndDelete({_id:req.params.id,user:req.user.id});
+        const deletedTodo = await Todo.findOneAndDelete({_id:req.params.id,userId:req.user.id});
         if (!deletedTodo) return res.status(404).json({ message: "Todo not found" });
         res.json({ message: "Deleted successfully" });
       } catch (err) {
